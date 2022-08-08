5f2e22f2eff92d52ed72bf4f2bcb7bbbaeaa3f84
Divyajyoti <divyajyoti182000@gmail.com>
2022-08-08 18:05:56 +0530

    Revision

--- DSA Course-Question/src/Mathematics/FactorialOfNumber.java
+++ DSA Course-Question/src/Mathematics/FactorialOfNumber.java
@@ -2,14 +2,25 @@ package Mathematics;
 
 public class FactorialOfNumber {
     public static void main(String args[]) {
-        int n = 5;
+        int n = 120;
         System.out.println("Factorial of the number: " + factorial(n));
+        System.out.println("Number of Digits: " + countNumber(factorial(n)));
     }
 
-    public static int factorial(int n) {
+    public static long factorial(int n) {
         if (n == 0) {
             return 1;
         }
         return n * factorial(n - 1);
+    }
+
+    public static long countNumber(long n) {
+        long count = 0;
+        while (n != 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+
     }
 }
